a33a301739adc2eefb00502950db5c129a536b95
luodongrong <luodongrong@duia.com>
2019-06-24 10:32:54 +0800

    配置文件绝对路径

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -277,6 +277,8 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        'container':path.resolve(paths.appSrc,'./container'),
+        'component':path.resolve(paths.appSrc,'./component'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
